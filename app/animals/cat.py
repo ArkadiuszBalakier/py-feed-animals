321ba3087ddb37dda37b8b022c96f32bcf544587
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-29 10:54:56 +0200

    implementing flake8 rules

--- app/animals/cat.py
+++ app/animals/cat.py
@@ -2,9 +2,14 @@ from app.animals.animal import Animal
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True, appetite: int = 3,):
+    def __init__(
+            self,
+            name: str,
+            is_hungry: bool = True,
+            appetite: int = 3
+    ) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod
-    def catch_mouse():
+    def catch_mouse() -> None:
         print("The hunt began!")
--- app/animals/dog.py
+++ app/animals/dog.py
@@ -2,7 +2,12 @@ from app.animals.animal import Animal
 
 
 class Dog(Animal):
-    def __init__(self, name:str, is_hungry:bool = True, appetite:int = 7) -> None:
+    def __init__(
+            self,
+            name : str,
+            is_hungry : bool = True,
+            appetite : int = 7
+    ) -> None:
         super().__init__(name, appetite, is_hungry)
 
     @staticmethod

7e4fa5988fb4c40776ec8b50b9891e88c99409b2
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-29 10:51:19 +0200

    implementing feed animals func and remove bug in dog class

--- app/animals/dog.py
+++ app/animals/dog.py
@@ -1,4 +1,7 @@
-class Dog:
+from app.animals.animal import Animal
+
+
+class Dog(Animal):
     def __init__(self, name:str, is_hungry:bool = True, appetite:int = 7) -> None:
         super().__init__(name, appetite, is_hungry)
 
--- app/main.py
+++ app/main.py
@@ -1,19 +1,9 @@
+from __future__ import annotations
+
 from app.animals.animal import Animal
 from app.animals.cat import Cat
+from app.animals.dog import Dog
 
-lion = Animal("simba", 25)
-lion.print_name()
-food_points = lion.feed()
-print(food_points)
-print(lion.is_hungry)
-print(lion.feed())
 
-cat = Cat("Cat")
-cat.print_name()
-cat.feed()
-
-cat2 = Cat("Cat2",False)
-cat2.print_name()
-points = cat2.feed()
-print(points)
-cat2.catch_mouse()
+def feed_animals(animals: list[Animal]) -> int:
+    return sum(animal.feed() for animal in animals)

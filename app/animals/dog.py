bd1a0cd23c08703a4f009a90ae5e851f96532876
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-29 10:41:17 +0200

    implementing dog class

---
+++ app/animals/dog.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self, name:str, is_hungry:bool = True, appetite:int = 7) -> None:
+        super().__init__(name, appetite, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print("The slippers delivered!")
